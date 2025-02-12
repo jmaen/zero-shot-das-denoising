ed3711f413e6d2b7b4067a071d163de5aa494c28
Jannik Mänzer <jmaenzer@t-online.de>
2025-02-12 13:38:20 +0100

    ECA experiments

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -3,3 +3,6 @@ from .prognet import ProgNet
 from .unet import UNet
 from .unet_mod import UNetMod
 from .unet_mod_1d import UNetMod1d
+from .fc_eca import FC_ECA
+from .fc_eca1 import FC_ECA1
+from .unet_mod_eca import UNetModECA
